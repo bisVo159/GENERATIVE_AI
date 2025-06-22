afbf9b1e05e1deb92d243fae4f1778d39730ae3e
bisVo159 <biswasanik159@gmail.com>
2025-06-22 12:31:31 +0530

    vector stores and retrievers

---
+++ LANGCHAIN_RAG/Retrievers/mmr.py
@@ -0,0 +1,32 @@
+# MMR-> maximum Marginal Relevance
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
+
+
+docs = [
+    Document(page_content="LangChain makes it easy to work with LLMs."),
+    Document(page_content="LangChain is used to build LLM based applications."),
+    Document(page_content="Chroma is used to store and search document embeddings."),
+    Document(page_content="Embeddings are vector representations of text."),
+    Document(page_content="MMR helps you get diverse results when doing similarity search."),
+    Document(page_content="LangChain supports Chroma, FAISS, Pinecone, and more."),
+]
+
+embedding_model=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+vectorstore=FAISS.from_documents(
+    documents=docs,
+    embedding=embedding_model
+)
+
+retriever = vectorstore.as_retriever(
+    search_type="mmr",                  
+    search_kwargs={"k": 3, "lambda_mult": 0.5}  # k = top results, lambda_mult = relevance-diversity balance
+)
+
+query = "What is langchain?"
+results = retriever.invoke(query)
+
+for i, doc in enumerate(results):
+    print(f"\n--- Result {i+1} ---")
+    print(doc.page_content)
